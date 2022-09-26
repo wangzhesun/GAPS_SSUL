156028c054d8717718b455847e7645b75a7b5720
wangzhesun <wangzhe.sun@outlook.com>
2022-09-26 13:39:50 -0500

    Update gaps_run_voc.sh

--- gaps_run_voc.sh
+++ gaps_run_voc.sh
@@ -11,11 +11,11 @@ FEWSHOT=True
 NUMSHOT=1
 MEMORY=300 # 300 # [0 (for SSUL), 100 (for SSUL-M)]
 
-python3 gaps_main.py --crop_val --overlap --data_root ${DATA_ROOT} --model deeplabv3_resnet101 \
+python3 gaps_main.py --crop_val --data_root ${DATA_ROOT} --model deeplabv3_resnet101 \
         --gpu_id 0 --lr ${LR} --batch_size ${BATCH} --train_epoch ${EPOCH} --loss_type ${LOSS} \
         --dataset ${DATASET} --task ${TASK} --lr_policy poly --pseudo --pseudo_thresh ${THRESH} \
         --freeze --bn_freeze --unknown --w_transfer --amp --mem_size ${MEMORY} \
         --few_shot ${FEWSHOT} --num_shot ${NUMSHOT} \
-        --ckpt ./checkpoints/deeplabv3_resnet101_voc_15-1-split3_step_0_disjoint.pth \
+        --ckpt ./checkpoints/deeplabv3_resnet101_voc_15-1-split2_step_0_disjoint.pth \
 
 ############  DO NOT forget to change the class old in tasks.py
