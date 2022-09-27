cf39ea3d2d11d76069690e47cae9935f8157b3b9
wangzhesun <wangzhe.sun@outlook.com>
2022-09-27 11:41:37 -0500

    Update gaps_run_coco.sh

--- gaps_run_coco.sh
+++ gaps_run_coco.sh
@@ -1,15 +1,15 @@
 DATA_ROOT=../data/COCO2017
 DATASET=coco
-TASK=15-1-split2
-FOLDING=2
+TASK=15-1-split3
+FOLDING=3
 EPOCH=50 #50
 BATCH=16 #32
 LOSS=bce_loss
 LR=0.01 # 0.01# try decreasing learning rate
 THRESH=0.7
 FEWSHOT=True
-NUMSHOT=5
-#NUMSHOT=1
+#NUMSHOT=5
+NUMSHOT=1
 MEMORY=500 # [0 (for SSUL), 100 (for SSUL-M)]
 
 ###### few shot step 1 - last
@@ -19,4 +19,4 @@ python gaps_main.py --data_root ${DATA_ROOT} --model deeplabv3_resnet101 --gpu_i
                     --pseudo_thresh ${THRESH} --freeze --bn_freeze --unknown --w_transfer --amp \
                     --mem_size ${MEMORY} \
                     --few_shot ${FEWSHOT} --num_shot ${NUMSHOT} \
-                    --ckpt ./checkpoints/deeplabv3_resnet101_coco_15-1-split2_step_0_disjoint.pth \
+                    --ckpt ./checkpoints/deeplabv3_resnet101_coco_15-1-split3_step_0_disjoint.pth \
